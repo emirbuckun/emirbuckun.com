59a92999c0f9ed4561a77ab72dc62c98092e35ed
Emir Büçkün <emirbuckun@hotmail.com>
2024-08-05 18:38:26 +0300

    Layout & Home Page - Shadcn UI components added - Next Themes installed and used - Tailwind typography installed and used - Main layout created - Home page created - Navbar created - Site configs added

---
+++ src/components/layouts/nav.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/layouts/theme-toggle";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+export function Navbar() {
+  return (
+    <header className="w-full bg-background pt-4">
+      <div className="container flex h-16 max-w-3xl items-center justify-between">
+        <Link href="/" className="font-bold">
+          {siteConfig.name}
+          <span className="sr-only">Home</span>
+        </Link>
+        <nav className="flex items-center space-x-1">
+          <Button variant="ghost" size="icon" className="size-7" asChild>
+            <Link
+              href={siteConfig.links.githubProfile}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubLogoIcon className="size-4" />
+              <span className="sr-only">GitHub</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" size="icon" className="size-7">
+            <Link
+              href={siteConfig.links.linkedinProfile}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInLogoIcon className="size-4" />
+              <span className="sr-only">LinkedIn</span>
+            </Link>
+          </Button>
+          <ThemeToggle />
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "emirbuckun",
+  description: "Personal website of Emir, built with Next.js.",
+  url: "https://emirbuckun.com",
+  links: {
+    github: "https://github.com/emirbuckun/emirbuckun.com",
+    githubProfile: "https://github.com/emirbuckun",
+    linkedinProfile: "https://www.linkedin.com/in/emirbuckun",
+  },
+};
